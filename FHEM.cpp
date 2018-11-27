6b59addea88ad9468547d56322992a0c4070f1df
kingmathers313 <pfeifferphilipp@hotmail.de>
2018-11-27 19:52:03 +0100

    Add files via upload

---
+++ FHEM.cpp
@@ -0,0 +1,87 @@
+// 
+// 
+// 
+
+#include "Arduino.h"
+#include "FHEM.h"
+#include <base64.h>
+#include <ESPHTTPClient.h>
+
+FHEM::FHEM(String Server, String User, String Password)
+{
+	FHEM_Server = Server;
+	FHEM_User = User;
+	FHEM_Password = Password;
+}
+
+FHEM::FHEM(String Server)
+{
+	FHEM_Server = Server;
+}
+
+String FHEM::LoadFromServer(String command)
+{
+	if (!CSRFTokenloaded)
+	{
+		LoadCSRFToken();
+	}
+
+	PrepareClient(FHEM_Server, command);
+
+	int httpCode = client.GET();
+
+	String result = "";
+
+	if (httpCode > 0) //Check the returning code
+	{
+		result = client.getString(); //Get the request response payload
+	}
+
+	client.end();   //Close connection
+
+	return result;
+}
+
+void FHEM::PrepareClient(String URL, String command)
+{
+	command.replace(" ", "%20");  //remove blank spaces. Other special characters are not regarded, these must be replaced before
+	URL = URL + "?XHR=1&cmd=" + command;
+	if (CSRFToken != "")
+	{
+		URL = URL + "&fwcsrf=" + CSRFToken;
+	}
+	PrepareClient(URL);
+}
+
+void FHEM::PrepareClient(String URL)
+{
+	if (FHEM_User != "")
+	{
+		String auth = FHEM_User + ":" + FHEM_Password;
+		auth = base64::encode(auth);
+		client.setAuthorization(auth.c_str());
+	}
+
+	client.begin(URL);
+}
+
+
+void FHEM::LoadCSRFToken()
+{
+	PrepareClient(FHEM_Server);
+
+	const char* headerNames[] = { "X-FHEM-csrfToken" };
+	client.collectHeaders(headerNames, sizeof(headerNames) / sizeof(headerNames[0]));
+
+	int httpCode = client.GET();
+	if (httpCode > 0)
+	{
+		if (client.hasHeader("X-FHEM-csrfToken"))
+		{
+			CSRFToken = client.header("X-FHEM-csrfToken");
+		}
+	}
+	client.end();
+
+	CSRFTokenloaded = true;
+}
---
+++ FHEM.h
@@ -0,0 +1,28 @@
+// FHEM.h
+
+#ifndef FHEM_h
+#define FHEM_h
+
+#include "Arduino.h"
+#include <ESPHTTPClient.h>
+
+class FHEM
+{
+public:
+	FHEM(String Server);
+	FHEM(String Server, String User, String Password);
+	String LoadFromServer(String command);
+private:
+	void PrepareClient(String URL);
+	void PrepareClient(String URL, String command);
+	void LoadCSRFToken();
+	HTTPClient client;
+	bool CSRFTokenloaded = false;
+	String CSRFToken = "";
+	String FHEM_Server = "";
+	String FHEM_User = "";
+	String FHEM_Password = "";
+};
+
+#endif
+
