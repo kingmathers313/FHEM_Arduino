146cdf55b32b0664af5621510d6eb12925b34095
Philipp Pfeiffer <17662199+kingmathers313@users.noreply.github.com>
2018-12-10 07:49:28 +0100

    bug fixes

--- src/FHEM.h
+++ src/FHEM.h
@@ -11,6 +11,7 @@ class FHEM
 public:
 	FHEM(String Server); //Server is required with port like this: http://192.168.1.1:8083/fhem
 	FHEM(String Server, String User, String Password); //Server is required with port like this: http://192.168.1.1:8083/fhem
+	FHEM(String Server, String Authentication); //auth is base64 encoded already
 	String LoadFromServer(String command);
 private:
 	void PrepareClient(String URL);
